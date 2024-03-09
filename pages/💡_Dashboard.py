a9999c8d721030210c2a7a0c91994bc28ec6db97
NandoAACF <nando.aacf88@gmail.com>
2024-03-09 11:28:22 +0700

    feat: add dashboard page

---
+++ pages/💡_Dashboard.py
@@ -0,0 +1,94 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv('housing.csv')
+    df.dropna(inplace=True)
+    return df
+
+df = load_data()
+
+st.title('🏠 California Housing Dashboard')
+
+st.sidebar.title('Filter')
+
+min_price = st.sidebar.slider('Min Price', int(df['median_house_value'].min()), int(df['median_house_value'].max()), int(df['median_house_value'].min()))
+
+max_price = st.sidebar.slider('Max Price', int(df['median_house_value'].min()), int(df['median_house_value'].max()), int(df['median_house_value'].max()))
+
+filtered_df = df[(df['median_house_value'] >= min_price) & (df['median_house_value'] <= max_price)]
+
+# Filter ocean proximity
+ocean_proximity = st.sidebar.multiselect('Ocean Proximity', filtered_df['ocean_proximity'].unique())
+
+if ocean_proximity:
+    filtered_df = filtered_df[filtered_df['ocean_proximity'].isin(ocean_proximity)]
+
+col1, col2, col3, col4 = st.columns(4)
+
+col1.metric('Total Houses', value=filtered_df.shape[0])
+col2.metric('Average House Price', value=int(filtered_df['median_house_value'].mean()))
+col3.metric('Average Income', value=(int(filtered_df['median_income'].mean())*10000))
+col4.metric('Average House Age', value=int(filtered_df['housing_median_age'].mean()))
+
+col1.metric('Average Population', value=int(filtered_df['population'].mean()))
+col2.metric('Average Households', value=int(filtered_df['households'].mean()))
+col3.metric('Average Rooms', value=int(filtered_df['total_rooms'].mean()))
+col4.metric('Average Bedrooms', value=int(filtered_df['total_bedrooms'].mean()))
+
+
+st.subheader('🗺️ Map Distribution of Median House Price')
+fig = px.scatter_mapbox(filtered_df, lat='latitude', lon='longitude',
+                        hover_name='ocean_proximity',
+                        color='median_house_value',
+                        size='population',
+                        zoom=4.3, height=550, color_continuous_scale=px.colors.sequential.Magma_r)
+
+fig.update_layout(mapbox_style='open-street-map')
+fig.update_layout(title='Map Distribution of Median House Price')
+
+st.plotly_chart(fig, theme='streamlit')
+
+
+st.subheader('💵 Median House Price & Median Income')
+plt.figure(figsize=(12, 5))
+plt.subplot(1, 2, 1)
+sns.histplot(filtered_df['median_house_value'], kde=True, bins=30)
+plt.title('Distribution of Median House Price')
+plt.xlabel('Median House Price')
+plt.ylabel('Frequency')
+
+plt.subplot(1, 2, 2)
+sns.histplot(filtered_df['median_income'], kde=True, bins=30, color='orange')
+plt.title('Distribution of Median Income')
+plt.xlabel('Median Income')
+plt.ylabel('Frequency')
+
+plt.tight_layout()
+st.pyplot(plt)
+
+st.subheader('📊 Median House Price by Age Category')
+
+filtered_df['age_category'] = pd.cut(filtered_df['housing_median_age'],
+                            bins=[0, 10, 20, 30, 40, 50, np.inf],
+                            labels=['0-10', '11-20', '21-30', '31-40', '41-50', '50+'])
+
+median_values_by_age = filtered_df.groupby('age_category')['median_house_value'].median().sort_index()
+
+plt.figure(figsize=(10, 5))
+
+palette = sns.color_palette('magma_r', len(median_values_by_age))
+
+median_values_by_age.plot(kind='bar', color=palette)
+
+plt.title('Median House Price by Age Category')
+plt.xlabel('Age Category')
+plt.ylabel('Median House Value')
+plt.xticks(rotation=0)
+
+st.pyplot(plt)
